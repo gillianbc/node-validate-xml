4a048c980c1b224a2b45fa01e5fd8f4f162750f7
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-10-10 20:24:20 +0100

    Added promise to cater for asynchronousification malarkey

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ var fs = require('fs');
 
 var xmlStr = fs.readFileSync('./resources/Demo.xml').toString();
 console.log(xmlStr);
+var errorCount = 0;
+var somefield = '';
+function doVal(str,xsdfile){
+  return new Promise(function(resolve,reject){
+    validator.validateXML(str, xsdfile, function(err, result) {
+       if (err){
+         reject(err);
+       } else {
+         resolve(result);
+       }
+    });
+  });
+}
 
-validator.validateXML(xmlStr, 'resources/Demo.xsd', function(err, result) {
-  if (err) {
-    console.log(err.message);
-  }
-  if (result.valid) {
-    console.log("File is valid");
-  } else {
-    console.log("File is not valid");
-  }
-});
+
+doVal(xmlStr,'resources/Demo.xsd')
+.then((result)=>{
+  console.log('All OK');
+  somefield = 'A';
+})
+.catch((err) => {
+  errorCount += 1;
+  console.log('Error 2 ' + errorCount);
+  somefield = 'N';
+})
+.then(() => {
+  console.log('Somefield ' + somefield);
+})
+
+
+console.log('Error 1 ' + errorCount);
